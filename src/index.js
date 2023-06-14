c69e005fec15212a705057de613d796e8126e6a5
Umair513 <soomroumair513@gmail.com>
2023-06-14 22:57:05 +0500

    Setup babel with express.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const express = require("express")
+// const express = require("express")
+import express from "express";
 
 const app = express()
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    res.send("Hello Poapl")
 })
 
 app.listen(port, () => {
